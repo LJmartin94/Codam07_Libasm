7fe272e610bf8ec33f08d283482ac24c3094d820
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-03-11 19:42:34 +0100

    Starting ft_strlen cleanly

--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -10,35 +10,12 @@
 ;#                                                                              #
 ;# **************************************************************************** #
 
-; DATA Registers (ABCD - X): EAX - AX - AH & AL
-; AX - primary accumulator (input/output for arithmetic operands)
-; BX - base register, can be used in indexed addressing
-; CX - count register, store e.g. loop count in iterative operations
-; DX - data register, also an input output register (used in conjunction with AX)
-
-; POINTER Registers (ISB - P): ESP - SP
-;
-
 section .data
-; The data section is used for declaring initialized data or constants. 
-; This data does not change at runtime.
-
-msg db 'Hello, world!', 0xa  ;string to be printed
-len equ $ - msg     ;length of the string
 
 section .bss
-; The bss section is used for declaring variables. 
+;len db
 
 section .text
-; The text section contains the actual code.
 
-	global _start     ;must be declared for linker (ld)
-_start:	            ;tells linker entry point
-   mov	edx,len     ;message length
-   mov	ecx,msg     ;message to write
-   mov	ebx,1       ;file descriptor (stdout)
-   mov	eax,4       ;system call number (sys_write)
-   int	0x80        ;call kernel
-	
-   mov	eax,1       ;system call number (sys_exit)
-   int	0x80        ;call kernel
+global ft_strlen
+ft_strlen:
