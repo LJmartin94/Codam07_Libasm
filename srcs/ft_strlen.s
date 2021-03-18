18ad9b80bddeb5106876d13b7afbc1d6b88cacb2
LJmartin94 <lindsayjamesmartin94@gmail.com>
2021-03-18 18:14:17 +0100

    Finished ft_strlen.s

--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -13,11 +13,19 @@
 section .data
 
 section .bss
-;len db
 
 section .text
 
 global _ft_strlen
 _ft_strlen:
-	mov rax, 0 ; Move value 0 to register AX
-	ret ; Return the value in rax
+	mov rcx, 0						; Initialise our counter to 0
+	loop:							; Find null terinator
+	;{
+		cmp byte [rdi+rcx], 0x0		; Test char of arg in RDI plus index of RCX is NULL
+		je match					; Leave loop if yes
+		inc rcx						; Otherwise increment our index - the counter register RCX
+		jmp loop					; and go back to start of loop
+	;}
+	match:
+	mov rax, rcx					; Move the index RCX into the return value register RAX
+	ret								; Return the value in RAX
