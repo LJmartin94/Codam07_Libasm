3acc14b0dc7d71723b0dcc8de7197b37042e89b5
limartin <lindsayjamesmartin94@gmail.com>
2021-03-28 21:19:19 +0200

    Got write working for linux, not sure if I need to allign the stack before calling the function however (seems to work fine either way) so leaving those two lines of code commented out for now.

--- srcs/linux/ft_write.s
+++ srcs/linux/ft_write.s
@@ -15,11 +15,13 @@ extern __errno_location
 ; Defining user-friendly register and syscall names:
 %define WRITE 1
 %define SYS rax
-
 %define RET rax
+
 %define FD rdi
 %define BUF rsi
 %define LEN rdx
+
+%define VAL rdi
 
 section .bss
 
@@ -27,20 +29,19 @@ section .text
 
 global ft_write
 ft_write:
-	mov SYS, WRITE	; Select the write system call
-	syscall			; Perform syscall
-	cmp RET, 0
-	jl error
-	ret 			; Return the value in rax
+	mov SYS, WRITE					; Select the write system call
+	syscall							; Perform syscall
+	cmp RET, 0						; Compare ret val to 0
+	jl error						; Jump if negative, indicates error
+	ret 							; Otherwise return the value in rax
 
 	error:
-	neg RET			; Negate return val of failed syscall
-	push RET		; Push value to stack (errno val)
-	;sub rsp, 8
-	call __errno_location	; Get errno location
-	;add rsp, 8
-	; Get stack val into loc provided by errno
-	pop rdi			; fetch errno value from stack into rdi
-	mov [RET], rdi	; move errno val into errno loc  
-	mov RET, -1		; Make return -1
-	ret
+	neg RET							; Negate return val of failed syscall
+	push RET						; Push current value to stack (errno val)
+	;sub rsp, 8						; Align stack before function call
+	call __errno_location wrt ..plt	; Get errno location, save ptr in RET
+	;add rsp, 8						; Re-align stack after function call
+	pop VAL							; Fetch errno val from stack into VAL
+	mov [RET], VAL					; Move VAL into errno loc pointed to by RET
+	mov RET, -1						; Make return -1
+	ret								; Return
